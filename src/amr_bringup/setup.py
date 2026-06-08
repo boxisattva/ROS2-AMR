345ac57f258389b2915180fba8f652c9645f2d51
Denis ZS <boxisattva@yandex.ru>
2026-06-08 12:36:03 +0300

    feat(amr_bringup): add talker/listener nodes
    - Talker: publishes String to /chatter every 1s
    - Listener: subscribes and logs messages
    - Verified with ros2 topic echo

---
+++ src/amr_bringup/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'amr_bringup'
+
+setup(
+    name=package_name,
+    version='0.1.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='denis',
+    maintainer_email='boxisattva@yandex.ru',
+    description='AMR bringup package: talker/listener demo',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = amr_bringup.talker:main',
+            'listener = amr_bringup.listener:main',
+        ],
+    },
+)
